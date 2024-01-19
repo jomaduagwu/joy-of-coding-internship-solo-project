1dd340454ecbb086f354f552b0a96a0deca7ba67
Ms Eko <jomaduagwu@gmail.com>
2024-01-19 14:31:31 -0600

    routing params

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,16 +1,5 @@
-// In this file, all Page components from 'src/pages` are auto-imported. Nested
-// directories are supported, and should be uppercase. Each subdirectory will be
-// prepended onto the component name.
-//
-// Examples:
-//
-// 'src/pages/HomePage/HomePage.js'         -> HomePage
-// 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
-
 import { Router, Route, Set } from '@redwoodjs/router'
-
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
-
 import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
 
 const Routes = () => {
@@ -23,6 +12,7 @@ const Routes = () => {
         <Route path="/posts" page={PostPostsPage} name="posts" />
       </Set>
       <Set wrap={BlogLayout}>
+        <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
       </Set>
--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -1,4 +1,8 @@
+import Article from 'src/components/Article'
+
 import type { ArticlesQuery } from 'types/graphql'
+
+// import { Link, routes } from '@redwoodjs/router'
 
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
@@ -25,13 +29,7 @@ export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
   return (
     <ul>
       {articles.map((article) => (
-        <article key={article.id}>
-          <header>
-            <h2>{article.title}</h2>
-          </header>
-          <p>{article.body}</p>
-          <div>Posted at: {article.createdAt}</div>
-        </article>
+        <Article key={article.id} article={article} />
       ))}
     </ul>
   )
